7c056c93220578e76e3402dcfbac8ec2b5bb1d5f
Rishi Bala <rishibala@rishis-mbp.lan>
2023-07-27 19:07:54 -0400

    notifyBot method

--- src/com/rishibala/Bot.java
+++ src/com/rishibala/Bot.java
@@ -81,14 +81,47 @@ public class Bot implements Runnable{
         for(Set<Order> matches : matchedOrders) {
             for(Order specificOrd : matches) {
                 if(specificOrd.botId() == botId) {
-                    notifyBot(specificOrd);
+                    notifyBot(matches);
                 }
             }
         }
     }
 
-    private void notifyBot(Order matchedOrder) {
-        out.println("Found match for Order: " + matchedOrder.toString());
-        orderBook.removeOrder(matchedOrder);
+    private void notifyBot(Set<Order> match) {
+        Object[] match1 = match.toArray();
+        Order p1 = (Order) match1[0];
+        Order p2 = (Order) match1[1];
+
+        Order bot1;
+        Order bot2;
+        Order buy;
+        Order sell;
+
+        if(p1.botId() == this.botId) {
+            bot1 = p1;
+            bot2 = p2;
+        } else {
+            bot1 = p2;
+            bot2 = p1;
+        }
+
+        if(p1.type().equals(Order.Type.BUY)) {
+            buy = p1;
+            sell = p2;
+        } else {
+            buy = p2;
+            sell = p1;
+        }
+
+        out.println("Found match for Order: " + bot1.toString());
+        out.println("Trading with Order: " + bot2.toString());
+
+        if(bot1.equals(buy)) {
+            user.updateStockAmt(sell.quantity());
+        } else {
+            user.updateStockAmt(sell.quantity() * -1);
+        }
+
+        orderBook.removeOrder(this.botId);
     }
 }
