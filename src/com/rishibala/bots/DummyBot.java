5911073f99cbfc8ce2c56c0ce8793ed307f4466d
Rishi Bala <rishibala@rishis-mbp.lan>
2023-07-29 16:42:31 -0400

    get data for DummyBot

---
+++ src/com/rishibala/bots/DummyBot.java
@@ -0,0 +1,93 @@
+package com.rishibala.bots;
+
+import com.rishibala.config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.*;
+
+public class DummyBot {
+
+    private static final String API_KEY = config.API_KEY;
+    public static void main(String[] args) {
+        String startDate = "2023-06-01";
+        String endDate = "2023-06-30";
+
+        Map<LocalDate, Double> data = getData(startDate, endDate);
+
+        for (LocalDate date : data.keySet()) {
+            double price = data.get(date);
+            System.out.println(date + ": " + price);
+        }
+
+    }
+
+    private static Map<LocalDate, Double> getData(String startDate, String endDate) {
+
+        Map<LocalDate, Double> data = new HashMap<>();
+        try {
+            String url = "https://api.twelvedata.com/time_series?symbol=AAPL" +
+                    "&interval=1day" +
+                    "&start_date=" + startDate +
+                    "&end_date=" + endDate +
+                    "&apikey=" + API_KEY;
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            String jsonResponse = response.toString();
+//            System.out.println(jsonResponse);
+
+            List<Integer> indexes = new ArrayList<>();
+            int temp = jsonResponse.indexOf("\"datetime\":\"");
+            while (temp >= 0) {
+                indexes.add(temp);
+                temp = jsonResponse.indexOf("\"datetime\":\"", temp + 1);
+            }
+
+            for (int index : indexes) {
+//                int closingIndex = jsonResponse.indexOf("\"close\":\"", index);
+
+//                while (closingIndex != -1) {
+                int endIndex = jsonResponse.indexOf("}", index);
+
+                if (endIndex == -1) {
+                    break;
+                }
+
+                String data1 = jsonResponse.substring(index, endIndex);
+                String[] dataItems = data1.split(",");
+
+//                System.out.println(Arrays.toString(dataItems));
+
+                String dateHalf = dataItems[0].substring(12, dataItems[0].lastIndexOf("\""));
+                LocalDate date = LocalDate.parse(dateHalf);
+
+                String closeHalf = dataItems[4].substring(9, dataItems[4].lastIndexOf("\""));
+                double price = Double.parseDouble(closeHalf);
+
+                data.put(date, price);
+//                    closingIndex = jsonResponse.indexOf("\"close\":\"", endIndex);
+//                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+}
